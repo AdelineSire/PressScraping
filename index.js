dfca7d8428ecffb6b8f2746397f7791abc99edf0
Adeline <adeline.sire@gmail.com>
2022-01-22 12:58:03 +0100

    Search settings and search action

--- index.js
+++ index.js
@@ -16,7 +16,22 @@ const start = async () => {
 		page.click('input[type="submit"]'),
 		page.waitForNavigation(),
 	]);
-	await Promise.all([page.goto(EUROPRESS_URL), page.waitForNavigation()]);
+
+	await Promise.all([
+		page.goto(EUROPRESS_URL),
+		page.waitForSelector('#DateFilter_DateRange'),
+	]);
+
+	// Search settings
+	await page.select('select#DateFilter_DateRange', '1');
+	await page.$eval('option[value="265293"]', (e) =>
+		e.setAttribute('selected', 'selected')
+	);
+
+	// Search
+	await page.focus('#Keywords');
+	await page.keyboard.type('Zemmour');
+	await page.click('#btnSearch');
 
 	// await browser.close();
 };
