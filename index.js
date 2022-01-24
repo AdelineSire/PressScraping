18c9f5f41459b6c6eab6be0c1d49da6ff38b504c
Adeline <adeline.sire@gmail.com>
2022-01-24 22:53:41 +0100

    Attempt to get the results from an array of keywords

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
+const fs = require('fs');
 const puppeteer = require('puppeteer');
-const { UNI_USERNAME, UNI_PASSWORD, UNI_URL, EUROPRESS_URL } = process.env;
 
-const start = async (keyword) => {
+const { UNI_USERNAME, UNI_PASSWORD, UNI_URL, EUROPRESS_URL } = process.env;
+const keywords = require('./keywords');
+
+const scrapArticles = async (keywords) => {
 	// Login
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
@@ -17,39 +20,65 @@ const start = async (keyword) => {
 		page.waitForNavigation(),
 	]);
 
-	await Promise.all([
-		page.goto(EUROPRESS_URL),
-		page.waitForSelector('#DateFilter_DateRange'),
-	]);
+	const getArticles = async (keyword) => {
+		// Go to the page and wait until loaded
+		await Promise.all([
+			page.goto(EUROPRESS_URL),
+			page.waitForSelector('#DateFilter_DateRange'),
+		]);
 
-	// Search settings
-	await page.select('select#DateFilter_DateRange', '1');
-	await page.$eval('option[value="265293"]', (e) =>
-		e.setAttribute('selected', 'selected')
-	);
+		// Search settings : "aujourd'hui", "presse nationale"
+		await page.select('select#DateFilter_DateRange', '1');
+		await page.$eval('option[value="265293"]', (e) =>
+			e.setAttribute('selected', 'selected')
+		);
 
-	// Search
-	await page.focus('#Keywords');
-	await page.keyboard.type(keyword);
+		// Enter keyword
+		await page.focus('#Keywords');
+		await page.keyboard.type(`TIT_HEAD=${keyword}`);
 
-	await Promise.all([
-		await page.click('#btnSearch'),
-		page.waitForSelector('.documentList'),
-	]);
+		// Search and wait for results
+		await Promise.all([
+			await page.click('#btnSearch'),
+			page.waitForSelector('.docListItem'),
+		]);
 
-	const evaluatedResults = await page.evaluate(() => {
-		const results = [];
-		document.querySelectorAll('.docListItem').forEach((el) => {
-			const newspaper = el.querySelector('.source-name').textContent;
-			const title = el.querySelector('.docList-links').textContent;
-			results.push({ newspaper, title });
+		// // Get the number of articles
+		// const pageCount = document.querySelector(
+		// 	'#documentLoadedCount'
+		// ).textContent;
+		// const totalCount = document.querySelector(
+		// 	'#documentLoadedCount'
+		// ).textContent;
+
+		// Get articles
+		const articles = await page.evaluate(() => {
+			const evaluatedArticles = [];
+			document.querySelectorAll('.docListItem').forEach((el) => {
+				const newspaper = el.querySelector('.source-name').textContent;
+				const title = el.querySelector('.docList-links').textContent;
+				results.push({ newspaper, title });
+			});
+			return evaluatedArticles;
 		});
-		return results;
+
+		// Push in results
+		results.push({ keyword, count, articles });
+	};
+
+	const results = [];
+
+	keywords.forEach((keyword) => {
+		getArticles(keyword);
 	});
 
-	console.log('evaluatedResults: ', evaluatedResults);
+	await browser.close();
 
-	// await browser.close();
+	return results;
 };
 
-start('Zemmour');
+const results = scrapArticles(keywords);
+console.log('typeof results: ', typeof results);
+
+// const data = JSON.stringify(results, null, 2);
+// fs.writeFile('./data', data);
---
+++ keywords.js
@@ -0,0 +1,19 @@
+// const keywords = [
+// 	'Arthaud',
+// 	'Poutou',
+// 	'Thouy',
+// 	'Lassalle',
+// 	'Dupont-Aignan',
+// 	'Roussel',
+// 	'Mélenchon',
+// 	'Hidalgo',
+// 	'Jadot',
+// 	'Taubira',
+// 	'Pécresse',
+// 	'Zemmour',
+// 	'Le Pen',
+// 	'Macron',
+// ];
+const keywords = ['Taubira', 'Pécresse'];
+
+module.exports = { keywords };
